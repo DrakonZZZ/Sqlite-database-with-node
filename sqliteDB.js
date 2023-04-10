298db559677fb7b81fd8f520a98820718526b180
Drakonz <124328116+DrakonZZZ@users.noreply.github.com>
2023-04-10 20:33:40 +0530

    add db file

--- sqliteDB.js
+++ sqliteDB.js
@@ -1,14 +1,15 @@
 const sql = require('sqlite3').verbose();
 const path = require('node:path');
 
-const db = new sql.Database(
-  path.join(__dirname, 'db', 'dog.json'),
-  sql.OPEN_CREATE,
-  (err) => {
-    if (err) throw err.message;
-    console.log('Connected to in-memory database');
-  }
-);
+const db = new sql.Database(path.join(__dirname, 'db', 'chinook.db'));
+const sqlQuery = `SELECT DISTINCT Name name FROM playlists
+           ORDER BY name`;
+db.all(sqlQuery, [], (err, rows) => {
+  if (err) throw err;
+  rows.forEach((row) => {
+    console.log(row.name);
+  });
+});
 
 db.close((err) => {
   if (err) throw err.message;
